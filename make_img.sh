de5928072b812ab57b60b5469a1a705877f83de7
Ruyi.zhang <www.zhangyi.studio@gmail.com>
2024-09-01 21:54:00 +0800

    amd qemu 运行kernel debug，vscode断不住，经排查是kernel地址随机化问题
    disable KASLR
    
    1、重新编译内核关闭地址随机化
    or
    2、或者在cmdline 里面直接添加 nokaslr
    
    Signed-off-by: Ruyi.zhang <www.zhangyi.studio@gmail.com>

--- make_img.sh
+++ make_img.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-image_name=rootfs.ext4
+platform=_amd64
+image_name=rootfs${platform}.ext4
 set -x
 rm -rf tmpfs ${image_name}.img
-dd if=/dev/zero of=${image_name}.img bs=1G count=10
+dd if=/dev/zero of=${image_name}.img bs=1G count=20
 mkfs.ext4 ${image_name}.img
 mkdir -p tmpfs
 mount ${image_name}.img tmpfs
-cp -rfp ./rootfs/* ./tmpfs/
+cp -rfp ./rootfs${platform}/* ./tmpfs/
 umount tmpfs
 e2fsck -p -f ${image_name}.img
-#resize2fs -M ${image_name}.img
+resize2fs -M ${image_name}.img
 #tar zcf ${image_name}.img.tar.gz ${image_name}.img
--- run_kernel_dbg.sh
+++ run_kernel_dbg.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 LROOT=$PWD
-export KERNEL_PATH=$LROOT/linux_dbg
+platform=_amd64
+export KERNEL_PATH=$LROOT/linux-dbg
 
-SMP="-smp 4"
+SMP="-smp 1"
 DBG="-s -S"
-
-
 
 run_qemu_ubuntu(){
 		qemu-system-x86_64 -m 2G\
-            -append "noinintrd console=ttyS0 root=/dev/vda rootfstype=ext4 rw loglevel=8" \
-			-drive if=none,file=rootfs.ext4.img,id=hd0 \
+            -append "noinintrd console=ttyS0 root=/dev/vda rootfstype=ext4 rw nokaslr loglevel=8" \
+			-drive if=none,file=rootfs${platform}.ext4.img,id=hd0 \
 			-device virtio-blk-pci,drive=hd0 \
 			-nographic $SMP -kernel $KERNEL_PATH/arch/x86/boot/bzImage \
 			$DBG
