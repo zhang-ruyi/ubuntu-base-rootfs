a64b3c169790a4aa7790b58247a045366a0912ce
Ruyi.zhang <www.zhangyi.studio@gmail.com>
2024-09-02 22:09:02 +0800

    fix devtmpfs:error mounting -2问题
    用普通用户制作根文件系统时部分文件拷贝不成功，会导致dev挂载失败
    因为chroot情况下安装软件默认是root账号，拷贝时用sudo或sudo执行
    脚本才能完整拷贝准备好的文件到根文件系统目录。
    
    Signed-off-by: Ruyi.zhang <www.zhangyi.studio@gmail.com>

--- make_img.sh
+++ make_img.sh
@@ -6,9 +6,9 @@ rm -rf tmpfs ${image_name}.img
 dd if=/dev/zero of=${image_name}.img bs=1G count=20
 mkfs.ext4 ${image_name}.img
 mkdir -p tmpfs
-mount ${image_name}.img tmpfs
-cp -rfp ./rootfs${platform}/* ./tmpfs/
-umount tmpfs
+sudo mount ${image_name}.img tmpfs
+sudo cp -rfp ./rootfs${platform}/* ./tmpfs/
+sudo umount tmpfs
 e2fsck -p -f ${image_name}.img
 resize2fs -M ${image_name}.img
 #tar zcf ${image_name}.img.tar.gz ${image_name}.img
